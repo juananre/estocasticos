a7ede151231ac82b7a14c3a4fb5a06d5d6bbe23f
juananre <78058130+juananre@users.noreply.github.com>
2022-10-31 01:30:34 -0500

    achu

--- estocasticos_unity/Assets/scripts/Master.cs
+++ estocasticos_unity/Assets/scripts/Master.cs
@@ -14,16 +14,43 @@ public class Master : MonoBehaviour
     [SerializeField] int contador_intentos = 5;
     [SerializeField] int contador_buenas;
     [SerializeField] int contador_malas;
+    [Header("contadores intento 1")]
+    [SerializeField] int contador_rojas1;
+    [SerializeField] int contador_verdes1;
+    [SerializeField] int contador_azules1;
+    [Header("contadores intento 2")]
+    [SerializeField] int contador_rojas2;
+    [SerializeField] int contador_verdes2;
+    [SerializeField] int contador_azules2;
+    [Header("contadores intento 3")]
+    [SerializeField] int contador_rojas3;
+    [SerializeField] int contador_verdes3;
+    [SerializeField] int contador_azules3;
+    [Header("contadores intento 4")]
+    [SerializeField] int contador_rojas4;
+    [SerializeField] int contador_verdes4;
+    [SerializeField] int contador_azules4;
 
-    [SerializeField] int contador_rojas;
-    [SerializeField] int contador_verdes;
-    [SerializeField] int contador_azules;
+    [Header("probabilidad")]
+    [SerializeField] int rango_probabilidad = 3;
 
-    [SerializeField] int rango_probabilidad = 4;
-
-    [SerializeField] TextMeshProUGUI texto_rojas;
-    [SerializeField] TextMeshProUGUI texto_azul;
-    [SerializeField] TextMeshProUGUI texto_verde;
+    [Header("intento 1")]
+    [SerializeField] TextMeshProUGUI texto_rojas1;
+    [SerializeField] TextMeshProUGUI texto_azul1;
+    [SerializeField] TextMeshProUGUI texto_verde1;
+    [Header("intento 2")]
+    [SerializeField] TextMeshProUGUI texto_rojas2;
+    [SerializeField] TextMeshProUGUI texto_azul2;
+    [SerializeField] TextMeshProUGUI texto_verde2;
+    [Header("intento 3")]
+    [SerializeField] TextMeshProUGUI texto_rojas3;
+    [SerializeField] TextMeshProUGUI texto_azul3;
+    [SerializeField] TextMeshProUGUI texto_verde3;
+    [Header("intento 4")]
+    [SerializeField] TextMeshProUGUI texto_rojas4;
+    [SerializeField] TextMeshProUGUI texto_azul4;
+    [SerializeField] TextMeshProUGUI texto_verde4;
+    [Header("tex")]
     [SerializeField] TextMeshProUGUI texto_intentos;
 
     [SerializeField] GameObject ganaste;
@@ -34,6 +61,10 @@ public class Master : MonoBehaviour
 
     bool terminar = false;
     bool verificar = false;
+    bool turno = false;
+
+    bool verde1 = true;
+    bool verde2 = true;
 
     // Start is called before the first frame update
     void Start()
@@ -45,10 +76,19 @@ public class Master : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        texto_rojas.text = "" + contador_rojas;
-        texto_verde.text = "" + contador_verdes;
-        texto_azul.text = "" + contador_azules;
+
+        texto_rojas1.text = "" + contador_rojas1;
+        texto_verde1.text = "" + contador_verdes1;
+        texto_azul1.text = "" + contador_azules1;
+
+        texto_rojas2.text = "" + contador_rojas2;
+        texto_verde2.text = "" + contador_verdes2;
+        texto_azul2.text = "" + contador_azules2;
+
+        texto_rojas3.text = "" + contador_rojas3;
+        texto_verde3.text = "" + contador_verdes3;
+        texto_azul3.text = "" + contador_azules3;
+
         texto_intentos.text = "" + contador_intentos;
 
 
@@ -94,6 +134,8 @@ public class Master : MonoBehaviour
             num_resutado = Random.Range(0, rango_probabilidad);
             verificar = true;
             contador_intentos--;
+            turno = true;
+
         }
        
     }
@@ -104,25 +146,46 @@ public class Master : MonoBehaviour
             case 0:
                 resultado.material.color = Color.green;
                 contador_buenas++;
-                contador_verdes++;
+                if (verde1 == true && turno == true)
+                {
+                    contador_verdes1++;
+                    verde1 = false;
+                    
+                }
+
+                if (verde1 == true && turno == true)
+                {
+                    contador_verdes2++;
+                    verde2 = false;
+                    
+                }
+                if (contador_verdes2 == 1 && verde2 == true)
+                {
+                    contador_verdes3++;
+                    
+                }
+                if (contador_verdes3 >= 1)
+                {
+                    contador_verdes4++;
+                }
                 break;
 
             case 1:
                 resultado.material.color = Color.blue;
                 contador_malas++;
-                contador_azules++;
+                contador_azules1++;
                 break;
 
             case 2:
                 resultado.material.color = Color.red;
                 contador_malas++;
-                contador_rojas++;
+                contador_rojas1++;
                 break;
 
             case 3:
                 resultado.material.color = Color.green;
                 contador_buenas++;
-                contador_verdes++;
+                contador_verdes1++;
                 break;
         }
     }
